00841f39d6e8a5b9d396c8aec5fc131c1de3afb1
Daniel Pérez <ing.dany@gmail.com>
2025-01-13 14:22:50 -0800

    Update Pakage .json and Locators.ts

--- test/specs/locators.ts
+++ test/specs/locators.ts
@@ -1,6 +1,6 @@
 
 describe("Locators Test Scenarios", () => {
-    it("should login with valid credentials", async () => {
+    it("Should login with valid credentials", async () => {
         await browser.url("https://demoqa.com/links");
     });
 
@@ -16,5 +16,51 @@ describe("Locators Test Scenarios", () => {
         expect(await responseLink).toHaveText("Link has responded with status 201 and status text Created");
     });
 
-    
+    it("CSS By Index Example", async () => {
+        const noContentLink = await $("#linkWraper p:nth-child(6) a");
+        const responseLink = await $("#linkResponse");
+
+        noContentLink.scrollIntoView();
+        noContentLink.click();
+
+        responseLink.scrollIntoView();
+        // expect(await responseLink.getText()).toContain("Link has responded with status 201 and status text Created");
+        expect(await responseLink).toHaveText("Link has responded with status 204 and status text No Content");
+    });
+
+    it("Partial LinkText", async () => {
+        const movedLink = await $("a=Moved");
+        const responseLink = await $("#linkResponse");
+
+        movedLink.scrollIntoView();
+        movedLink.click();
+
+        responseLink.scrollIntoView();
+        // expect(await responseLink.getText()).toContain("Link has responded with status 201 and status text Created");
+        expect(await responseLink).toHaveText("Link has responded with status 301 and status text Moved Permanently");
+    });
+
+    it("Partial Element Text", async () => {
+        const movedLink = await $("a*=Mov");
+        const responseLink = await $("#linkResponse");
+
+        movedLink.scrollIntoView();
+        movedLink.click();
+
+        responseLink.scrollIntoView();
+        // expect(await responseLink.getText()).toContain("Link has responded with status 201 and status text Created");
+        expect(await responseLink).toHaveText("Link has responded with status 301 and status text Moved Permanently");
+    });
+
+    it("XPath Example", async () => {
+        const xpathLink = await $("//*[@id='bad-request']");
+        const responseLink = await $("#linkResponse");
+
+        xpathLink.scrollIntoView();
+        xpathLink.click();
+
+        responseLink.scrollIntoView();
+        // expect(await responseLink.getText()).toContain("Link has responded with status 201 and status text Created");
+        expect(await responseLink).toHaveText("Link has responded with status 400 and status text Bad Request");
+    });
 });
